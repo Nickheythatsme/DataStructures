236d0c16364268bc7ef231b3129203cd258e9cc4
nick grout <njgrout@gmail.com>
2018-02-12 12:49:38 -0800

    modified:   Makefile 	modified:   main.cpp 	deleted:    queue.cpp 	deleted:    queue.h

--- threaded/queue/main.cpp
+++ threaded/queue/main.cpp
@@ -1,27 +1,47 @@
-#include "queue.h"
+#include "worker.h"
 #include <iostream>
 
 using namespace std;
-typedef int worker(int *array, int len);
 
-int w(int *array, int len)
+// Declare the type of arguments to be used
+struct array_args
 {
-    for(auto i = 0; i < len; ++i)
-        cout << array[i] << ", ";
+    int* array;
+    int len;
+    const char *message;
+};
+
+// Declare the type of function to be called
+typedef int& func(array_args *a);
+
+
+int& test_func(array_args *a)
+{
+    cout << "Displaying: " << a->message << endl;
+    for(auto i = 0; i < a->len; ++i)
+        cout << a->array[i] << ", ";
     cout << endl;
-    return len;
-}
-
-
-void test_pass_func(worker *w)
-{
-    int a[] = {1,2,3};
-    int len = 3;
-    cout << "Len: " << w(a, len) << endl;
+    return a->len;
 }
 
 int main(int argc, char **argv)
 {
-    test_pass_func(w);
+    int array[] = {1,2,3,4};
+    array_args a = {
+        array,
+        4,
+        "this message"};
+
+    worker<func,array_args> w(test_func);
+
+    try{
+        w.run(&a);
+        w.run(&a);
+    }
+    catch(err_running e)
+    {
+        cout << e << endl;
+    }
+
     return 0;
 }
